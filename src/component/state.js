543995caa9847a919259d5bfeac62bc026be604e
Yamin Alam Sarker <yaminalam27@gmail.com>
2020-10-07 20:47:17 +0600

    state with multiple array & object

--- src/component/state.js
+++ src/component/state.js
@@ -6,10 +6,17 @@ class State extends Component {
     constructor() {
         super()
 
+
         this.state= {
 
             name:"Sarker",
-            age: '28',
+            age: ["12", '13', '14', '15'],
+            weight: {
+
+                class7: ['45', '50'],
+                class10: '70',
+                class12: '75'
+            }
         }
     }
 
@@ -18,7 +25,8 @@ class State extends Component {
         return <div>
 
             <h1>Name: {this.state.name} </h1>
-            <h1>age: {this.state.age}</h1>
+            <h1>age: {this.state.age[2]}</h1>
+            <h1>age: {this.state.weight.class7[1]}</h1>
 
         </div>
 
