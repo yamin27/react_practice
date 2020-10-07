1445202650ca16850d31f7d5642322ef15cea4c4
Yamin Alam Sarker <yaminalam27@gmail.com>
2020-10-07 17:47:17 +0600

    Class & funtional Component Practice

---
+++ src/component/hello.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Hello(){
+
+    return (
+
+        <h1>hello, i am from functional component</h1>
+
+    );
+}
+
+export default Hello;
---
+++ src/component/welcome.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+
+class Welcome extends Component{
+
+    render() {
+
+        return <h1> I am from class component</h1>
+    }
+
+}
+
+export default Welcome;
