51a63b7a3456ccf6fda65fc2f26feb06a459a25d
Yamin Alam Sarker <yaminalam27@gmail.com>
2020-10-10 15:25:32 +0600

    Form & onsubmit state

--- src/component/Form.js
+++ src/component/Form.js
@@ -6,24 +6,34 @@ class Form extends Component {
         super();
 
         this.state= {
-            name: " "
+            username: " "
         }
     }
 
     onChangeHandler=(event)=>{
+        //
+        // var newName = event.target.value;
+        // this.setState({name:newName})
 
-        var newName = event.target.value;
-        this.setState({name:newName})
+        var myname= event.target.name;
+        var myvalue = event.target.value;
 
+       this.setState({[myname]:myvalue})
+
+    }
+
+    onSubmitHandler=()=>{
+
+        alert(this.state.username)
     }
 
     render() {
         return (
             <div>
-                <form>
+                <form onSubmit={this.onSubmitHandler}>
                     <p>My First Form By ReactJs </p>
-                    <p>{this.state.name}</p>
-                    <input onChange={this.onChangeHandler} type="text" placeholder="Your Name"></input>
+                    <p>{this.state.username}</p>
+                    <input onChange={this.onChangeHandler} name="username" type="text" placeholder="Your Name"></input>
                     <br/>
                     <input type="submit" value="Submit"></input>
                 </form>
